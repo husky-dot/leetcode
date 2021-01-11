f0b9f1de297311f507e8eef8a2636ded51667b0f
qq449245884 <449245884@qq.com>
2021-01-12 00:32:06 +0800

    666

---
+++ 1.js
@@ -0,0 +1,53 @@
+function (nums) {
+  let ans = [];
+  const len = nums.length;
+  if(nums == null || len < 3) return ans;
+  nums.sort((a, b) => a - b); // 排序
+  for (let i = 0; i < len ; i++) {
+      if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+      if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
+      let L = i+1;
+      let R = len-1;
+      while(L < R){
+          const sum = nums[i] + nums[L] + nums[R];
+          if(sum == 0){
+              ans.push([nums[i],nums[L],nums[R]]);
+              while (L<R && nums[L] == nums[L+1]) L++; // 去重
+              while (L<R && nums[R] == nums[R-1]) R--; // 去重
+              L++;
+              R--;
+          }
+          else if (sum < 0) L++;
+          else if (sum > 0) R--;
+      }
+  }        
+  return ans;
+}
+
+
+
+let res = []
+if (nums === null || nums.length < 3) return res
+nums.sort((a, b) => a - b)
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) break
+    if (i > 0 && nums[i] === nums[i+1]) continue
+    let L = i + 1
+    let R = nums.length - 1
+    while(L < R) {
+        const sum = nums[i] + nums[L] + nums[R]
+        if (sum === 0 ) {
+          res.push([nums[i], nums[L], nums[R]])
+          while(L < R && nums[L] === nums[L+1]) L++
+          while(L < R && nums[R] === nums[R-1]) R-- 
+          L++
+          R--
+        } else if (sum < 0) {
+            L++
+        } else if (sum > 0) {
+            R--
+        }
+    } 
+}
+return res
