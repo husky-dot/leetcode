670ce176fd8918f672e79bb8aa1297d7e4620aee
qq449245884 <449245884@qq.com>
2021-01-24 18:21:02 +0800

    666

--- 1.js
+++ 1.js
@@ -1,18 +1,2 @@
 
-Array.prototype._map = function (fn, thisArg = []) {
-    const result = []
 
-    this.reduce((pre, cur, index, arr) => {
-        result.push(fn.call(this.thisArg, cur, index, arr))
-    }, 0)
-
-    return result;
-}
-
-const arr = [1,2,3,4,5]
-
-const res = arr._map((item) => {
-    return item * 2
-})
-
-console.log(res)
